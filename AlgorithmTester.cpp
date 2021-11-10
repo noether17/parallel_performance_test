eb3b691051158a05e7f7ef423e16be705f8b118f
Jonathan White <jawh@umich.edu>
2021-11-09 20:18:47 -0700

    Added outline for test_performance implementation.

--- AlgorithmTester.cpp
+++ AlgorithmTester.cpp
@@ -10,5 +10,23 @@ auto test_performance(Algorithm alg,
                       int block_size = default_block_size) -> ns
 {
     // TODO
+    // Need function to parse Algorithm and return function pointer
+    //   --may also return number of inputs needed
+    // Then generate input vector(s)
+    // Different threads need a reference to same vectors(s) and
+    //   a thread offset / stride
+
+    // Determine algorithm
+
+    // create input vector(s)
+
+    // create vector of threads
+
+    // start timer
+    // start threads
+    // join threads
+    // end timer
+
+    // return execution time
 }
 } // namespace AT
